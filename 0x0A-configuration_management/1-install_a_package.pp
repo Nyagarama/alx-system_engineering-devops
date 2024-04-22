a2c248da60ea4e428e8a79e0b38f6fd2059654c7
Nyagarama <mochristopher27@gmail.com>
2024-04-22 04:11:46 -0700

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 # Using Puppet, install flask from pip3
+package { 'python':
+  ensure  => 'present',
+}
 
 package { 'Werkzeug':
-  ensure   => '2.1.1',
+  ensure   => '2.1.1'
+  provider => 'pip3',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
